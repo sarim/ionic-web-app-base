2484f7e68ae7e1b03359728a95cccf4e51f87b0b
Sarim Khan <sarim2005@gmail.com>
2014-07-16 21:32:21 +0600

    updated gulpfile
    added a server and removed unrelated tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,10 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
 var sh = require('shelljs');
-
-var paths = {
-  sass: ['./scss/**/*.scss']
-};
-
-gulp.task('default', ['sass']);
-
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
-});
-
-gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
-});
+var connect = require('connect');
+var chalk = require('chalk');
+var opn = require('opn');
 
 gulp.task('install', ['git-check'], function() {
   return bower.commands.install()
@@ -47,4 +24,18 @@ gulp.task('git-check', function(done) {
     process.exit(1);
   }
   done();
+});
+
+gulp.task('server', function(next) {
+  var server = connect(),
+      host = 'localhost',
+      port = process.env.DEVPORT || 4000;
+
+  server
+    .use(connect.static('www'))
+    .listen(port, next)
+    .on('listening', function () {
+        console.log(chalk.green('Started dev server on http://' + host + ':' + port));
+        opn('http://' + host + ':' + port);
+    });
 });
